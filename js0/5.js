77d843a3c52b24dbec881eb1510b4ed6353ca4c2
Aaron Yeung <aaronyeung9@gmail.com>
2022-03-18 20:06:21 -0700

    solves problem 5

--- js0/5.js
+++ js0/5.js
@@ -8,7 +8,16 @@
  */
 
 const solution = (num1, num2, num3) => {
-  return 0
+  if (num1>num2){
+    if (num1>num3){
+      return num1
+    }
+    else return num3
+  }
+  else if (num2>num3){
+    return num2  
+  }
+  return num3
 }
 
 module.exports = {
