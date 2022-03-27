f3a58ff455ba05222c563589da3810565e015803
Aaron Yeung <aaronyeung9@gmail.com>
2022-03-26 23:44:51 -0700

    fixes problem 8 suggestion

--- js1/8.js
+++ js1/8.js
@@ -8,13 +8,13 @@
  */
 
 const solution = (inp, letter, counter = 0, i = 0) => {
-  if (i<inp.length){
-    if(inp[i] === letter) {
+  if (i>=inp.length){
+    return counter
+  }
+  if(inp[i] === letter) {
     return solution (inp, letter, counter+1, i+1)
     }
     return solution (inp, letter, counter, i+1)
-  }
-  return counter
 }
 
 module.exports = {
