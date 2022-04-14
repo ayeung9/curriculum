ab8ff7af54b1c25b530296ab803bd233874bfdb2
Aaron Yeung <aaronyeung9@gmail.com>
2022-04-14 08:50:43 -0700

    solves problem 7

--- js3/7.js
+++ js3/7.js
@@ -5,8 +5,12 @@
 */
 
 const solution = () => {
-  Object.prototype.map = function (cb) {
-    return []
+  Object.prototype.map = function (cb, key = Object.keys(this), value = Object.values(this), i = 0, result = [] ) {
+    if (i >= key.length){
+      return result
+    }
+    result.push(cb(key[i], value[i], i))
+    return this.map (cb, key, value, i+1, result)
   }
 }
 
