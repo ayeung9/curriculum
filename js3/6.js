9293bf7250a335a59f89791a98fec8ff5cdbe49b
Aaron Yeung <aaronyeung9@gmail.com>
2022-04-14 08:25:23 -0700

    solves problem 6

--- js3/6.js
+++ js3/6.js
@@ -5,7 +5,21 @@
 */
 
 const solution = (arr) => {
-  return []
+  const numberOfElements = arr.reduce((acc, e) => {
+    acc[e] = (acc[e] || 0) + 1
+    return acc
+  },{})
+  
+  const duplicates = (array, i = 0, result = []) => {
+    if (i >= array.length){
+      return result
+    }
+    if (array[i][1] > 1){
+      result.push(parseInt(array[i][0]))
+    }
+    return duplicates(array, i+1, result)
+	}
+  return duplicates(Object.entries(numberOfElements))
 }
 
 module.exports = {
