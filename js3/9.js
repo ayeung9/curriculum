e3ea354b80ef41ee71da03e3ddf39b5893a74561
Aaron Yeung <aaronyeung9@gmail.com>
2022-04-16 18:57:27 -0700

    fixes problem 9

--- js3/9.js
+++ js3/9.js
@@ -2,21 +2,17 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then(response => {
-        return response.json()
-    })
+    .then(r => r.json())
     .then(data => {
         const fetchPromises = data.results.map(pokemon => {
             return fetch(pokemon.url)
-            .then(details => {
-                return details.json()
-            })
+            .then(r => r.json())
         })
         return Promise.all(fetchPromises)
     })
     .then(list => {
         const string = list.reduce((acc, e) => {
-            return `${acc}<div><h1>${e.name}</h1><img src="${e.sprites.front_shiny}"/>`
+            return `${acc}<div><h1>${e.name}</h1><img src="${e.sprites.front_shiny}"/></div>`
         },'')
         fs.writeFile("./9.html", string, () => {})
     })
